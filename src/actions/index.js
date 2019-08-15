3463bce7a5e13b088c210d6365810484bbb86e47
Shelby Diamond <Shelbydiamond518@gmail.com>
2019-08-14 19:21:21 -0700

    feeling stuck. just commit-ing code in case

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,19 +1,21 @@
-export const AddFeature = "ADD_FEATURE"
-
 export const AddFeature = item => {
-  console.log(item)
   return {
     type: "ADD_FEATURE",
     payload: item
   }
 }
 
-export const REMOVE_FEATURE = "REMOVE_FEATURE"
-
 export const RemoveFeature = item => {
-  console.log(item)
   return {
     type: "REMOVE_FEATURE",
     payload: item
   }
+}
+
+export const addAdditionalPrice = item => {
+  return { type: "ADD_ADDITIONAL_PRICE", payload: item }
+}
+
+export const removeAdditionalPrice = item => {
+  return { type: "REMOVE_ADDITIONAL_PRICE", payload: item }
 }
